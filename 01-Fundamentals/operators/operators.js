eec3bf2ac9e0fccc9a39342d610dfa1aa4d4a3d2
Eduardo Marques <eduardomar1703@gmail.com>
2024-08-29 20:23:00 -0300

    Aprendendo sobre os operadores de javascript

---
+++ 01-Fundamentals/operators/operators.js
@@ -0,0 +1,38 @@
+// Operadores aritmeticos
+let num1 = 10;
+let num2 = 17;
+
+console.log(num1 + num2); // adição
+console.log(num1 - num2); // subtração
+console.log(num1 * num2); // multiplicação
+console.log(num1 / num2); // divisão
+console.log(num1 % num2); // módulo da divisão
+console.log(num1 ** num2); // exponenciação
+num1++; // incremento
+num1--; // decremento
+
+// Operadores de atribuição
+
+console.log(num1 = 11); // atribui 
+console.log(num1 += num2); // num1 = num1 + num2
+console.log(num1 -= num2); // num1 = num1 - num2
+console.log(num1 *= num2); // num1 = num1 * num2
+console.log(num1 /= num2); // num1 = num1 / num2
+console.log(num1 %= num2); // num1 = num1 % num2
+console.log(num1 **= num2); // num1 = num1 ** num2
+
+// Operadores de comparação
+
+console.log(11 == '11'); // compara os valores
+console.log(11 === '11'); // compara os valores e o tipo
+console.log(11 != '11'); // compara os valores
+console.log(11 !== '11'); // compara os valores e o tipo
+console.log(17 > 8); // maior que
+console.log(17 >= 8); // maior ou igual 
+console.log(17 < 8); // maior que
+console.log(8 <= 8); // menor ou igual 
+
+// Operadores Lógicos
+console.log(true && true); // 'true' quando todos são verdadeiros
+console.log(false || true); // 'true' quando pelo menos um for verdadeiro
+console.log(!true); // inverte a expressão
