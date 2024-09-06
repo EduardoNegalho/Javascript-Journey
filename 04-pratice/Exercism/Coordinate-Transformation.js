e56b46b642198a413827a06a6911688b3f35fa1d
Eduardo Marques <eduardomar1703@gmail.com>
2024-09-06 09:26:33 -0300

    alterando o nome da pasta

---
+++ 04-pratice/Exercism/Coordinate-Transformation.js
@@ -0,0 +1,15 @@
+function translate2d(dx, dy) {
+    throw new Error('Implement the translate2d function');
+}
+
+function scale2d(sx, sy) {
+    throw new Error('Implement the scale2d function');
+}
+
+function composeTransform(f, g) {
+    throw new Error('Implement the composeTransform function');
+}
+
+function memoizeTransform(f) {
+    throw new Error('Implement the memoizeTransform function');
+}
