7f064326c5801309ff64e21c2f043e09e177f802
Eduardo Marques <eduardomar1703@gmail.com>
2024-09-18 10:15:21 -0300

    aprendendo sobre o babel

---
+++ 05-others/babel/dist/hello.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var name = 'Eduardo';
+var age = 20;
+console.log("Hello, ".concat(name, ", you have ").concat(age, " years old?"));
---
+++ 05-others/babel/src/hello.js
@@ -0,0 +1,4 @@
+const name = 'Eduardo'
+const age = 20
+
+console.log(`Hello, ${name}, you have ${age} years old?`);
